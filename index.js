da60ccbf2a9db1940bc6d438ef251313afe5d312
ChilenoSika <jaum97@yahoo.com.br>
2018-11-20 02:43:21 -0200

    Adding Nodemon, Middleware

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-/* const http = require('http');
-
-http.createServer((req, res) => {
-    console.log(req);
-    return res.end('Hello World');
-}).listen(3000); */
-
 const express = require("express");
 
 const app = express();
+
+const logMiddleware = (req, res, next) => {
+    console.log(
+        `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+    );
+
+    return next();
+};
+
+app.use(logMiddleware);
 
 app.get('/', (req, res) => {
     return res.send(`Welcome, ${req.query.name}`);
 });
 
-app.get('/login', (req, res) => {
-    return res.send('Login');
-});
-
 app.get('/name/:name', (req, res) => {
     return res.json({
-        message: `Welcome, ${req.params.name}`
+        message: `Welcomey, ${req.params.name}`
     });
 });
 app.listen(3000);
