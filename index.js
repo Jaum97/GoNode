1b12dc16452c5f899b4de31f2a9622ad463b7caf
ChilenoSika <jaum97@yahoo.com.br>
2018-11-20 02:15:05 -0200

    Learning Express

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const http = require('http');
+/* const http = require('http');
 
 http.createServer((req, res) => {
     console.log(req);
     return res.end('Hello World');
-}).listen(3000);
+}).listen(3000); */
+
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send(`Welcome, ${req.query.name}`);
+});
+
+app.get('/login', (req, res) => {
+    return res.send('Login');
+});
+
+app.get('/name/:name', (req, res) => {
+    return res.json({
+        message: `Welcome, ${req.params.name}`
+    });
+});
+app.listen(3000);
